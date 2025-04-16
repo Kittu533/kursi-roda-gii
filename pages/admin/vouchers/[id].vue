0176e8e51dff3a6a4aa8d42abd557f3f0c60e8bb
Ryobi <robiatmaja7@gmail.com>
2025-04-16 23:36:25 +0700

    refactor: remove unused voucher detail and list components

--- pages/admin/vouchers/[id].vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div><ProductDetail /></div>
-</template>
-
-<script setup lang="ts">
-import ProductDetail from "~/components/product/product-detail.vue";
-</script>
--- pages/admin/vouchers/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div><VoucherList /></div>
-</template>
-
-<script setup lang="ts">
-import VoucherList from "~/components/voucher/voucher-list.vue";
-</script>
