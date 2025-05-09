616d9479679d3933a57a2fc75cec11e25cc84aa3
Ryobi <robiatmaja7@gmail.com>
2025-05-10 03:23:01 +0700

    chore: comment out mock data and related code in complaint API and types

--- types/complaint.ts
+++ types/complaint.ts
@@ -1,30 +1,30 @@
-export interface Complaint {
-    id: string;
-    complaintId: string;
-    customerId: string;
-    transactionId: string;
-    type: string;
-    description: string;
-    startDate: string;
-    completionDate: string;
-    status: 'Open' | 'Forwarded' | 'Resolved' | 'Rejected';
-}
+// export interface Complaint {
+//     id: string;
+//     complaintId: string;
+//     customerId: string;
+//     transactionId: string;
+//     type: string;
+//     description: string;
+//     startDate: string;
+//     completionDate: string;
+//     status: 'Open' | 'Forwarded' | 'Resolved' | 'Rejected';
+// }
 
-export interface ComplaintPagination {
-    currentPage: number;
-    totalPages: number;
-    total: number;
-    itemsPerPage: number;
-}
+// export interface ComplaintPagination {
+//     currentPage: number;
+//     totalPages: number;
+//     total: number;
+//     itemsPerPage: number;
+// }
 
-export interface ComplaintFilter {
-    page?: number;
-    complaintId?: string;
-    customerId?: string;
-    transactionId?: string;
-    type?: string;
-    status?: string;
-    startDate?: string;
-    endDate?: string;
-    itemsPerPage?: number;
-}
+// export interface ComplaintFilter {
+//     page?: number;
+//     complaintId?: string;
+//     customerId?: string;
+//     transactionId?: string;
+//     type?: string;
+//     status?: string;
+//     startDate?: string;
+//     endDate?: string;
+//     itemsPerPage?: number;
+// }
